692ba90487000126d8bd3c6d5455b4412f1fefde
Abdullah Alsharif <adalah.sh00@gmail.com>
2023-05-17 11:59:37 +0300

    fix AllPoetry class component

--- src/components/allPoetry.jsx
+++ src/components/allPoetry.jsx
@@ -1,44 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 import { Component } from "react";
 import { createServer } from "miragejs";
 import { data } from "../data";
-import Card from './poteryCard';
+import Card from "./poteryCard";
 
 let server = createServer();
 server.get("/azouha", data);
 
 export default class AllPoetry extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: null, 
-        };
-    }
-    componentDidMount() {
-        axios.get("/azouha")
-            .then(({data}) => {
-                this.setState({ data })
-            })
-            .catch(console.log)
-    }
-
-    render() {
-        const { data } = this.state;
-        console.log(data?.users)
-        return (
-            <>
-                {data?.users.map((user) => (
-                    <Card />
-                    // <li key={user.id}>
-                    //     <h2>{user.title}</h2>
-                    //     {user.poetry.split("@").map((e, i) => (
-                    //         <p key={i}>{e}</p>
-                    //     ))}
-                    //     <br />
-                    //     <br />
-                    // </li>
-                ))}
-            </>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { data } = this.props;
+    return (
+      <>
+        {data &&
+          data.map((item, index) => {
+            return <Card key={index} data={item} />;
+          })}
+      </>
+    );
+  }
 }
