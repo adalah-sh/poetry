8549ec7f75cdf20cb3c200160c9549101cfe7bf1
momenmarwan <momenalgaga82@gmail.com>
2023-05-17 09:50:55 +0300

    make a nav setciom componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-
 import './App.css'
-
-import { useState, useEffect } from "react";
-import { createServer } from "miragejs";
-import { data } from "./data";
 import AllPoetry from './components/allPoetry';
-let server = createServer();
-server.get("/azouha", data);
+import  NavBar  from './components/navbar';
 
 const App = () => {
-  let [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch("/azouha")
-      .then((res) => res.json())
-      .then((json) => {
-        setUsers(json.users);
-      });
-  }, []);
 
   return (
-    <ul>
-      <AllPoetry />
-    </ul>
+    <>
+      <NavBar />
+      {/* <AllPoetry /> */}
+    </>
   );
 };
 
