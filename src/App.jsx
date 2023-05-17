e46214e45c3dd777e788e8cd954f5d01419fcab0
Abdullah Alsharif <adalah.sh00@gmail.com>
2023-05-17 11:33:39 +0300

    6-fix-fetch-style-and-start-with-poetry-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css'
-import AllPoetry from './components/allPoetry';
+// import AllPoetry from './components/allPoetry';
 import  NavBar  from './components/navbar';
+import Poetry from './components/poetry';
+import { fetch } from './fetch';
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch().then((data) => {
+      console.log("dataF", data["data"]);
+      setData(data["data"]);
+    });
+  }, []);
 
   return (
     <>
       <NavBar />
-      {/* <AllPoetry /> */}
+      <Poetry data={data[5]} />
     </>
   );
 };
