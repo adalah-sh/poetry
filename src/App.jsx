6570c5c505c684166487b8f24cfbaefe13975813
Abdullah Alsharif <adalah.sh00@gmail.com>
2023-05-17 12:12:04 +0300

    fix again

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import './App.css'
 import AllPoetry from './components/allPoetry';
-import Card from './components/poteryCard'
 import  NavBar  from './components/navbar';
 import Poetry from './components/poetry';
 import { fetch } from './fetch';
 import { useState, useEffect } from 'react';
 
 const App = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
   useEffect(() => {
     fetch().then((data) => {
-      console.log("dataF", data["data"]);
       setData(data["data"]);
     });
   }, []);
 
   return (
     <>
+      <NavBar />
       <AllPoetry data={data} />
-      {/* <NavBar />
-      <Poetry data={data[5]} /> */}
+      {/*<Poetry data={data[5]} /> */}
     </>
   );
 };
